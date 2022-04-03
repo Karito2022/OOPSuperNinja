5e93ceb4696eb87ef0f1ab88436fad50c34c6dbb
Karito2022 <reyesbarrerakaren@gmail.com>
2022-04-03 14:52:36 -0500

    Finalizado

---
+++ ninja.js
@@ -0,0 +1,44 @@
+class Ninja {
+    constructor(nombre, salud) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.velocidad = 3;
+        this.fuerza = 3;
+    }
+
+    sayName() {
+        console.log(this.nombre);
+    }
+
+    showStats() {
+        console.log(
+        `Nombre: ${this.nombre}, fuerza: ${this.fuerza}, velocidad:${this.velocidad}, salud:${this.salud}`
+        );
+    }
+
+    drinkSake() {
+        this.salud += 10;
+    }
+}
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name, 200);
+        this.velocidad = 10;
+        this.fuerza = 10;
+        this.sabiduria = 10;
+    }
+
+    speakWisdom() {
+        super.drinkSake();
+        console.log(
+        "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+        );
+    }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+  // -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+  // -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
